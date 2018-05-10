433cc82ad54d68b821dff3e1e9935fe92758f49f
flying-linux <flying-linux@linux211.com>
2018-05-10 02:21:53 +0000

    Optimize the configuration

--- agent.sh
+++ agent.sh
@@ -4,11 +4,10 @@ crtPath=$(cd "$(dirname "$0")"; pwd)
 action=$1
 #sh unique_hosts.sh
 
-echo "The action is :"$action
 
 case $action in
 agent_install|agent_uninstall|proxy_install)
     ansible-playbook -i hosts -t $action agent.yml -K ;;
 *)
-   echo 'the action should be one of [agent_install|agent_uninstall|proxy_install]'
+   echo -e "\033[31mThe action should be one of :\033[0m""\033[32m [agent_install|agent_uninstall|proxy_install] \033[0m"  
 esac
