f2e71346da5ecd19c3c77e381a5c55957617765d
root <root@b80a510d75cc.(none)>
2018-05-10 01:38:22 +0000

    basic function

--- agent.sh
+++ agent.sh
@@ -4,19 +4,11 @@ crtPath=$(cd "$(dirname "$0")"; pwd)
 action=$1
 #sh unique_hosts.sh
 
-echo $action
+echo "The action is :"$action
 
-if [ "$action" == "install" ] ; then
-    #ansible-playbook agent.yml --extra-vars "ansible_ssh_user=root" -i $crtPath/hosts -K -k
-    ansible-playbook agent.yml --extra-vars "ansible_ssh_user=root" -i $crtPath/hosts 
-fi
-
-if [ "$action" == "init_disk" ] ; then
-    ansible-playbook agent.yml --extra-vars "ansible_ssh_user=root" -i $crtPath/hosts -t init_disk -K -k
-fi
-
-if [ "$action" == "init_sys" ]; then
-    ansible-playbook agent.yml --extra-vars "ansible_ssh_user=root" -i $crtPath/hosts -t init_sys -K -k
-fi
-
-
+case $action in
+agent_install|agent_uninstall|proxy_install)
+    ansible-playbook -i hosts -t $action agent.yml -K ;;
+*)
+   echo 'the action should be one of [agent_install|agent_uninstall|proxy_install]'
+esac
